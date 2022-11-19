bffe2d54a20784c258769cb0c364c95f029b81ef
senaa0 <senakaradag480@gmail.com>
2022-11-19 11:24:56 +0300

    Update odev.py

--- odev.py
+++ odev.py
@@ -2,7 +2,22 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import mysql.connector
 from tkinter import *
+mydb=mysql.connector.connect(host="93.89.225.112", user="pehozgun_admina", password="Admin5050", database="pehozgun_verita")
+cursor=mydb.cursor()
+query="select * from musteri"
+cursor.execute(query)
+rows = cursor.fetchall()
+def asd (rows):
+    ttk.Treeview.delete(*ttk.Treeview.get_children())
+    for i in rows:
+        ttk.Treeview.insert("", "end", values=i) 
 
+
+def clear():
+    query = "select * from musteri"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    asd(rows)
 def GetValue(event):
     e1.delete(0, END)
     e2.delete(0, END)
@@ -13,14 +28,13 @@ def GetValue(event):
     e7.delete(0, END)
     row_id = listBox.selection()[0]
     select = listBox.set(row_id)
-    e1.insert(0,select['id'])
-    e2.insert(0,select['cariadi'])
-    e3.insert(0,select['vno'])
-    e4.insert(0,select['vergidairesi'])
-    e5.insert(0,select['adres'])
-    e6.delete(0, select['telefon'])
-    e7.delete(0, select['sehir'])
-    
+    e1.insert(0, select['id'])
+    e2.insert(0, select['cariadi'])
+    e3.insert(0, select['vno'])
+    e4.insert(0, select['vergidairesi'])
+    e5.insert(0, select['adres'])
+    e6.insert(0, select['telefon'])
+    e7.insert(0, select['sehir'])
 
 
 def Add():
@@ -32,28 +46,33 @@ def Add():
     telefon = e6.get()
     sehir = e7.get()
 
-    mysqldb=mysql.connector.connect(host="93.89.225.112",user="pehozgun_admina",password="Admin5050",database="pehozgun_verita")
-    mycursor=mysqldb.cursor()
+    mysqldb = mysql.connector.connect(host="93.89.225.112", user="pehozgun_admina", password="Admin5050", database="pehozgun_verita")
+    mycursor = mysqldb.cursor()
 
     try:
-       sql = "INSERT INTO  musteri (id,cariadi,vno,vergidairesi,adres,telefon,sehir) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-       val = (id,cariadi,vno,vergidairesi,adres,telefon,sehir)
-       mycursor.execute(sql, val)
-       mysqldb.commit()
-       lastid = mycursor.lastrowid
-       messagebox.showinfo("information", "Başarıyla eklendi...")
-       e1.delete(0, END)
-       e2.delete(0, END)
-       e3.delete(0, END)
-       e4.delete(0, END)
-       e5.delete(0, END)
-       e6.delete(0, END)
-       e7.delete(0, END)
-       e1.focus_set()
+        sql = "INSERT INTO  musteri (id,cariadi,vno,vergidairesi,adres,telefon,sehir) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (id, cariadi, vno, vergidairesi, adres, telefon, sehir)
+        mycursor.execute(sql, val)
+        mysqldb.commit()
+        lastid = mycursor.lastrowid
+        messagebox.showinfo("information", "Başarıyla Eklendi...")
+        e1.delete(0, END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+        e5.delete(0, END)
+        e6.delete(0, END)
+        e7.delete(0, END)
+        e1.focus_set()
+        
+        cursor.execute(query,(id, cariadi, vno, vergidairesi, adres, telefon, sehir))
+        clear()
+
+        asd()
     except Exception as e:
-       print(e)
-       mysqldb.rollback()
-       mysqldb.close()
+        print(e)
+        mysqldb.rollback()
+        mysqldb.close()
 
 
 def update():
@@ -64,71 +83,81 @@ def update():
     adres = e5.get()
     telefon = e6.get()
     sehir = e7.get()
-    mysqldb=mysql.connector.connect(host="93.89.225.112",user="pehozgun_admina",password="Admin5050",database="pehozgun_verita")
-    mycursor=mysqldb.cursor()
+    mysqldb = mysql.connector.connect(
+        host="93.89.225.112", user="pehozgun_admina", password="Admin5050", database="pehozgun_verita")
+    mycursor = mysqldb.cursor()
 
     try:
-       sql = "Update  musteri set cariadi= %s,vno= %s,vergidairesi= %s,adres= %s,telefon= %s,sehir= %s where id= %s"
-       val = (cariadi,vno,vergidairesi,adres,telefon,sehir,id)
-       mycursor.execute(sql, val)
-       mysqldb.commit()
-       lastid = mycursor.lastrowid
-       messagebox.showinfo("information", "Başarıyla Güncellendi...")
+        sql = "Update  musteri set cariadi= %s,vno= %s,vergidairesi= %s,adres= %s,telefon= %s,sehir= %s where id= %s"
+        val = (cariadi, vno, vergidairesi, adres, telefon, sehir, id)
+        mycursor.execute(sql, val)
+        mysqldb.commit()
+        lastid = mycursor.lastrowid
+        messagebox.showinfo("information", "Başarıyla Güncellendi...")
 
-       e1.delete(0, END)
-       e2.delete(0, END)
-       e3.delete(0, END)
-       e4.delete(0, END)
-       e5.delete(0, END)
-       e6.delete(0, END)
-       e7.delete(0, END)
-       e1.focus_set()
+        e1.delete(0, END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+        e5.delete(0, END)
+        e6.delete(0, END)
+        e7.delete(0, END)
+        e1.focus_set()
+        cursor.execute(query,(cariadi, vno, vergidairesi, adres, telefon, sehir, id))
+        clear()
 
     except Exception as e:
 
-       print(e)
-       mysqldb.rollback()
-       mysqldb.close()
-       
+        print(e)
+        mysqldb.rollback()
+        mysqldb.close()
+
 
 def delete():
     id = e1.get()
 
-    mysqldb=mysql.connector.connect(host="93.89.225.112",user="pehozgun_admina",password="Admin5050",database="pehozgun_verita")
-    mycursor=mysqldb.cursor()
+    mysqldb = mysql.connector.connect(
+        host="93.89.225.112", user="pehozgun_admina", password="Admin5050", database="pehozgun_verita")
+    mycursor = mysqldb.cursor()
 
     try:
-       sql = "delete from musteri where id = %s"
-       val = (id,)
-       mycursor.execute(sql, val)
-       mysqldb.commit()
-       lastid = mycursor.lastrowid
-       messagebox.showinfo("information", "Başarıyla silindi...")
+        sql = "delete from musteri where id = %s"
+        val = (id,)
+        mycursor.execute(sql, val)
+        mysqldb.commit()
+        lastid = mycursor.lastrowid
+        messagebox.showinfo("information", "Başarıyla silindi...")
 
-       e1.delete(0, END)
-       e2.delete(0, END)
-       e3.delete(0, END)
-       e4.delete(0, END)
-       e5.delete(0, END)
-       e6.delete(0, END)
-       e7.delete(0, END)
-       e1.focus_set()
+        e1.delete(0, END)
+        e2.delete(0, END)
+        e3.delete(0, END)
+        e4.delete(0, END)
+        e5.delete(0, END)
+        e6.delete(0, END)
+        e7.delete(0, END)
+        e1.focus_set()
+        cursor.execute(query,(id))
+        clear()
 
     except Exception as e:
 
-       print(e)
-       mysqldb.rollback()
-       mysqldb.close()
-def show():
-        mysqldb = mysql.connector.connect(host="93.89.225.112",user="pehozgun_admina",password="Admin5050",database="pehozgun_verita")
-        mycursor = mysqldb.cursor()
-        mycursor.execute("SELECT id,cariadi,vno,vergidairesi,adres,telefon,sehir FROM musteri")
-        records = mycursor.fetchall()
-        print(records)
+        print(e)
+        mysqldb.rollback()
+        mysqldb.close()
 
-        for i, (id, cariadi, vno, vergidairesi, adres, telefon, sehir) in enumerate(records, start=1):
-            listBox.insert("", "end", values=(id, cariadi, vno, vergidairesi, adres, telefon, sehir))
-            mysqldb.close()
+
+def show():
+    mysqldb = mysql.connector.connect(
+        host="93.89.225.112", user="pehozgun_admina", password="Admin5050", database="pehozgun_verita")
+    mycursor = mysqldb.cursor()
+    mycursor.execute("SELECT * FROM musteri")
+    records = mycursor.fetchall()
+    print(records)
+
+    for i, (id, cariadi, vno, vergidairesi, adres, telefon, sehir) in enumerate(records, start=1):
+        listBox.insert("", "end", values=(id, cariadi, vno,vergidairesi, adres, telefon, sehir))
+        mysqldb.close()
+
 
 root = Tk()
 root.geometry("1000x500")
@@ -170,12 +199,15 @@ e6.place(x=450, y=40)
 e7 = Entry(root)
 e7.place(x=450, y=70)
 
-Button(root, text="Ekle",command = Add,height=3, width= 13,bg="green",).place(x=30, y=130)
-Button(root, text="Güncelle",command = update,height=3, width= 13,bg="blue",).place(x=140, y=130)
-Button(root, text="Sil",command = delete,height=3, width= 13,bg="red",).place(x=250, y=130)
+Button(root, text="Ekle", command=Add, height=3,
+       width=13, bg="green",).place(x=30, y=130)
+Button(root, text="Güncelle", command=update, height=3,
+       width=13, bg="blue",).place(x=140, y=130)
+Button(root, text="Sil", command=delete, height=3,
+       width=13, bg="red",).place(x=250, y=130)
 
 cols = ('id', 'cariadi', 'vno', 'vergidairesi', 'adres', 'telefon', 'sehir')
-listBox = ttk.Treeview(root, columns=cols, show='headings' )
+listBox = ttk.Treeview(root, columns=cols, show='headings')
 
 for col in cols:
     listBox.heading(col, text=col)
@@ -183,6 +215,6 @@ for col in cols:
     listBox.place(x=100, y=250)
 
 show()
-listBox.bind('<Double-Button-1>',GetValue)
+listBox.bind('<Double-Button-1>', GetValue)
 
 root.mainloop()
